570eeee4de8a19e8d14355cac20fffc7a89068af
Steven de Cleene <decleene.steven@gmail.com>
2020-12-06 13:50:10 +0100

    Day 6

--- src/main/java/com/stevecorp/codingcontest/adventofcode/common/InputReader.java
+++ src/main/java/com/stevecorp/codingcontest/adventofcode/common/InputReader.java
@@ -2,8 +2,10 @@ package com.stevecorp.codingcontest.adventofcode.common;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class InputReader {
@@ -17,5 +19,20 @@ public class InputReader {
         } catch (final Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static <T> List<List<T>> group(final List<T> input, final Predicate<T> separate) {
+        final List<List<T>> output = new ArrayList<>();
+        List<T> intermediate = new ArrayList<>();
+        for (final T inputElement : input) {
+            if (separate.test(inputElement)) {
+                output.add(intermediate);
+                intermediate = new ArrayList<>();
+            } else {
+                intermediate.add(inputElement);
+            }
+        }
+        output.add(intermediate);
+        return output;
     }
 }
