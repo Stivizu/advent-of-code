e6422c8c5e62c1f045515cf8b6431ba0d4fa3731
Steven de Cleene <decleene.steven@gmail.com>
2020-12-08 01:05:42 +0100

    Day 7

--- src/main/java/com/stevecorp/codingcontest/adventofcode/common/InputReader.java
+++ src/main/java/com/stevecorp/codingcontest/adventofcode/common/InputReader.java
@@ -3,7 +3,11 @@ package com.stevecorp.codingcontest.adventofcode.common;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -21,6 +25,17 @@ public class InputReader {
         }
     }
 
+    public static <T, U> Map<T, U> parseFileMap(final String inputFilePath, final Function<String, Map.Entry<T, U>> mapper) {
+        try {
+            return Files.readAllLines(Paths.get(InputReader.class.getClassLoader().getResource(inputFilePath).toURI()))
+                    .stream()
+                    .map(mapper)
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static <T> List<List<T>> group(final List<T> input, final Predicate<T> separate) {
         final List<List<T>> output = new ArrayList<>();
         List<T> intermediate = new ArrayList<>();
@@ -33,6 +48,18 @@ public class InputReader {
             }
         }
         output.add(intermediate);
+        return output;
+    }
+
+    public static <T> Map<T, Set<T>> reverseMap(final Map<T, Set<T>> input) {
+        final Map<T, Set<T>> output = new HashMap<>();
+        input.forEach((key, value) -> {
+            for (final T element : value) {
+                final Set<T> intermediate = output.containsKey(element) ? output.get(element) : new HashSet<>();
+                intermediate.add(key);
+                output.put(element, intermediate);
+            }
+        });
         return output;
     }
 }
