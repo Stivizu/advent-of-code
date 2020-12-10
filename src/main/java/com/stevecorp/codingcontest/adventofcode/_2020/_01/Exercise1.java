8e0ce1d767f28633de6c4cecdc10f3cc663abdcf
Steven de Cleene <decleene.steven@gmail.com>
2020-12-10 20:32:25 +0100

    Day 1

--- src/main/java/com/stevecorp/codingcontest/adventofcode/_2020/_01/Exercise1.java
+++ src/main/java/com/stevecorp/codingcontest/adventofcode/_2020/_01/Exercise1.java
@@ -10,9 +10,12 @@ public class Exercise1 {
         final List<Integer> input = parseFile("2020/01/input.txt", Integer::parseInt);
         for (int element1Index = 0; element1Index < input.size() - 1; element1Index++) {
             for (int element2Index = element1Index + 1; element2Index < input.size(); element2Index++) {
-                final int elementSum = input.get(element1Index) + input.get(element2Index);
+                final int element1 = input.get(element1Index);
+                final int element2 = input.get(element2Index);
+                final int elementSum = element1 + element2;
                 if (elementSum == 2020) {
-                    System.out.println(input.get(element1Index) * input.get(element2Index));
+                    final int elementMultiplication = element1 * element2;
+                    System.out.println("The two elements (" + element1 + ", " +  element2 + ") multiplied give as result: " + elementMultiplication);
                     System.exit(1);
                 }
             }
--- src/main/java/com/stevecorp/codingcontest/adventofcode/_2020/_01/Exercise2.java
+++ src/main/java/com/stevecorp/codingcontest/adventofcode/_2020/_01/Exercise2.java
@@ -11,9 +11,13 @@ public class Exercise2 {
         for (int element1Index = 0; element1Index < input.size() - 2; element1Index++) {
             for (int element2Index = element1Index + 1; element2Index < input.size() - 1; element2Index++) {
                 for (int element3Index = element2Index + 1; element3Index < input.size(); element3Index++) {
-                    final int elementSum = input.get(element1Index) + input.get(element2Index) + input.get(element3Index);
+                    final int element1 = input.get(element1Index);
+                    final int element2 = input.get(element2Index);
+                    final int element3 = input.get(element3Index);
+                    final int elementSum = element1 + element2 + element3;
                     if (elementSum == 2020) {
-                        System.out.println(input.get(element1Index) * input.get(element2Index) * input.get(element3Index));
+                        final int elementMultiplication = element1 * element2 * element3;
+                        System.out.println("The three elements (" + element1 + ", " +  element2 + ", " + element3 + ") multiplied give as result: " + elementMultiplication);
                         System.exit(1);
                     }
                 }
